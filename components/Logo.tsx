4f7d2d025f2d4c54f202163fbb927fa080f1ecdc
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2024-05-26 18:37:19 -0500

    Fixed logo missing

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Logo() {
     return (
         <Link href='/' className='flex flex-row gap-2 items-center'>
-            <img className='w-6 h-auto' src='/logos/spricalabs-logo-1.png'/>
+            <img className='w-6 h-auto' src='/logos/sprica-labs-logo-lt.png'/>
             <span className='font-semibold text-xl md:text-2xl'>Sprica Labs</span>
         </Link>
     )
