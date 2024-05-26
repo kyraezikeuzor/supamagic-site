5a9efec0b01cea9453bcb1d386c5dd599a29ceba
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2024-05-26 18:35:10 -0500

    Tiny tweaks to line height

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
           <AnimationMorphGradient>
             <section className='z-50 w-full flex flex-col gap-5 m-5 p-8 md:mx-[15vw] lg:p-12 lg:mx-[30vw] lg:my-[20vh] 
             bg-[--clr-base] rounded-3xl border border-[--clr-base-accent] shadow-lg'>
-              <h1 className='leading-[1.1]'>Sprica Labs is a UI/UX design studio making imaginative interfaces.</h1>
-              <p className='text-lg md:text-xl text-[--clr-grey-base] font-medium'>
+              <h1 className='leading-[1.2] lg:leading-[1.1]'>Sprica Labs is a UI/UX design studio making imaginative interfaces.</h1>
+              <p className='leading-[1.4] text-lg md:text-xl text-[--clr-grey-base] font-medium'>
                 Sprica turns ideas into websites, apps, and digital products for businesses, personal brands, and you.
               </p>
               <div className='flex flex-row gap-3 mt-5'>
