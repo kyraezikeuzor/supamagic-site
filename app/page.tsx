2422a6a1c2c2a3fe049d287242613aeb6cbb0f4b
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2024-05-27 09:33:20 -0500

    Tweaked coloring

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <header id='hero h-screen'>
           <AnimationMorphGradient>
             <section className='z-50 w-full flex flex-col gap-5 m-5 p-8 md:p-12 md:mx-[15vw] lg:p-12 lg:mx-[25vw] lg:my-[20vh] 2xl:mx-[30vw]
-            bg-[--clr-base] rounded-3xl border border-[--clr-base-accent] drop-shadow-sm'>
+            bg-[--clr-base-accent] rounded-3xl border border-[--clr-base-accent] drop-shadow-sm'>
               <h1 className='leading-[1.1]'>Sprica Labs is a UI/UX design studio making imaginative interfaces.</h1>
               <p className='leading-[1.4] text-lg md:text-xl text-[--clr-grey-base] font-medium'>
                 Sprica turns ideas into websites, apps, and digital products for businesses, personal brands, and you.
